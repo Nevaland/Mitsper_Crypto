70ff575b1ada0510933175c53f60b54e4095f564
Nevaland <deanh0716@gmail.com>
2019-04-29 13:55:17 +0900

    First Commit

---
+++ src/Crypto/Mitsper.java
@@ -0,0 +1,25 @@
+package Crypto;
+
+public class Mitsper {
+	private ChessObject c_object;
+	
+	public Mitsper(String key, String content) {
+		this.c_object = new ChessObject(key, content);
+	}
+
+	public void keySet(String key) {
+		this.c_object.setKey(key);
+	}
+	public void contentSet(String content) {
+		this.c_object.setBlock(content);
+	}
+	
+	public String encrypt() {
+		c_object.checkmate();	// sample
+		return "TEST";	// sample
+	}
+	public String decrypt() {
+		c_object.checkmate();	// sample
+		return "TEST";	// sample
+	}
+}
