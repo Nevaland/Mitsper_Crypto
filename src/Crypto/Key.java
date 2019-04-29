687ef196f747916629be8f95b0b55776453213ba
Nevaland <deanh0716@gmail.com>
2019-04-30 00:37:32 +0900

    Set sample values

--- src/Crypto/Key.java
+++ src/Crypto/Key.java
@@ -1,17 +1,18 @@
 package Crypto;
 
 public class Key {
-	private String key;
+	private String[] key = new String[12];
 	
-	public Key(String content){
-		this.key = content;
+	public Key(String key){
+		generate(key);
+	}
+	public void generate(String key) {
+		// Sample
+		for(int i=0;i<10;i++)
+			this.key[i] = "00010203040506070809101112131415";
 	}
 	
-	public String getKey() {
-		return this.key;
-	}
-	
-	public void nextKey() {
-		
+	public String getKey(int round) {
+		return this.key[round];
 	}
 }
