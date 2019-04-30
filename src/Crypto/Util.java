890f091d9135664b05524991daf7d68e81033b2a
Nevaland <deanh0716@gmail.com>
2019-04-30 21:25:33 +0900

    Modify functions

--- src/Crypto/Util.java
+++ src/Crypto/Util.java
@@ -1,6 +1,6 @@
 package Crypto;
 
-public class Util {
+public class Util {	
 	public String str2hex(String str) {
 		String hex = "";
 		
@@ -12,10 +12,13 @@ public class Util {
 	}
 	public String hex2str(String hex) {
 		String str = "";
-
-		int intVal = Integer.parseInt(hex, 16);
-		char charVal = (char) intVal;
-		str += charVal;
+//		System.out.println("TEST: "+hex);
+		for(int i = 0; i<hex.length(); i+=2){
+			int intVal = Integer.parseInt(hex.substring(i, i+2), 16);
+			char charVal = (char) intVal;
+			str += charVal;
+		}
+//		System.out.println("TEST: "+str);
 		return str;
 	}
 }
