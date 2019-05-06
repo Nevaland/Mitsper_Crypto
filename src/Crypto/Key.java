7988983881e5613661a4f45f10c285b50d9877e0
Seeeeerial <wlfl1311@naver.com>
2019-05-06 17:12:00 +0900

    주석처리되있던 부분 삭제

--- src/Crypto/Key.java
+++ src/Crypto/Key.java
@@ -5,9 +5,9 @@ import java.lang.Integer;
 
 public class Key
 {
-	private String[] key = new String[10];			//12������ key �迭
+	private String[] key = new String[10];			//12占쏙옙占쏙옙占쏙옙 key 占썼열
 	private String CBox = new String();			//chessbox
-	private String constantBox = new String();		//ù key ������ ���� ������ �����
+	private String constantBox = new String();		//첫 key 占쏙옙占쏙옙占쏙옙 占쏙옙占쏙옙 占쏙옙占쏙옙占쏙옙 占쏙옙占쏙옙占�
     private ConstantNode head = null;
 
     public class ConstantNode
@@ -75,14 +75,6 @@ public class Key
 
 	private void generateCBox(String key)
     {
-		/*this.CBox="000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
-				+ "202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f"
-				+ "404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f"
-				+ "606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f"
-				+ "808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9f"
-				+ "a0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebf"
-				+ "c0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedf"
-				+ "e0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";*/
 		Util util = new Util();
 		long num_key = 0;
 		String hexKey = util.str2hex(key);
@@ -115,10 +107,7 @@ public class Key
 
 	private void generateKey(String key)
     {
-		// Sample
-		//for(int i=0;i<10;i++)
-		//	this.key[i] = "00010203040506070809101112131415";
-        int i;
+		int i;
         Util util = new Util();
         String keyChanged = util.str2hex(key);
         String[] hexKey = new String[4];
@@ -225,7 +214,6 @@ public class Key
 		                			Integer.parseInt(roundKey[j].substring(k, k+2), 16)^Integer.parseInt(hexKey[j].substring(k, k+2), 16));
         				}
         				roundKey[j] = xor_key;
-//        				System.out.println(roundKey[j]);
         			}
         		}
         	}
